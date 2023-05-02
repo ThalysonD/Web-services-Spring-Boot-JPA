17ef996ef0a864b9f5e2231dd7ff63f38c984b8f
Thalyson Dornelas <thalysondornelasgomes14@gmail.com>
2023-05-02 13:56:29 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -19,7 +19,6 @@ import jakarta.persistence.Table;
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
